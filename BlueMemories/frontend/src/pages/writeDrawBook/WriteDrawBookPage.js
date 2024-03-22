7f6232b808fc6e3d7222d6607e1d57953d6f6cc9
jyun <groovy-sky-jyun@>
2024-03-23 01:53:04 +0900

    WriteNovelPage.js react작성 -> 마우스 focus에따라 이미지 생성 버튼 비/활성화 적용, textarea추가 //페이지 번호 추가해야함.

--- BlueMemories/frontend/src/pages/writeDrawBook/WriteDrawBookPage.js
+++ BlueMemories/frontend/src/pages/writeDrawBook/WriteDrawBookPage.js
@@ -1,51 +1,120 @@
 import {Helmet} from "react-helmet";
 import styled from 'styled-components';
-import { LoggedInNavigationBar } from '../../components/NavigationBar';
-import { BookListItemContainer } from '../../components/BookList';
-import { StoargeTextBtnContainer } from '../../components/StorageTextBtn';
+import { WriteMenuBar } from '../../components/WriteMenuBar';
 
 const Wrapper = styled.div`
     width:100%;
     height:100vh;
     display: flex;
+    background-color: #FBF5E4;
     flex-direction:column;
-    background-image: url("/resourcesPng/storagePage/background_mountain.png"),
-    linear-gradient(rgba(255, 242, 211, 1),rgba(255,242,211,0.84),rgba(255,242,211,0.62),rgba(255,242,211,0.41));
-    background-size: cover;
+    align-items: center; /* 수직 중앙 정렬 */
 `;
-const ContentContainer=styled.div`
-    height: 80%;
-    width: auto;
+
+const BodyContainer=styled.div`
+    width: 80%;
+    height:100%;
+    margin-bottom:1%;
+    /*item정렬*/
     display: flex;
-    justify-content: center;
-    margin-top: 3%;
+    justify-content: space-between;/* 수평  정렬 */
+    flex-direction: row;
+    align-items: center; /* 수직 중앙 정렬 */
 `;
-//-------------------------------------------------------------
-
-//가운데 아이템 컨테이너 베이지배경
-const ListBackgroundImg=styled.div`
-    width:47%;
-    height: 34vw;
-    background-image: url("/resourcesPng/storagePage/storage_list_background.png");
+const BeforePageBtn = styled.button`
+    background: url("/resourcesPng/writeNovelPage/beforePageBtn.png") no-repeat;
     background-size: contain;
-    background-position: center;
-    background-repeat: no-repeat;
-    align-items: center;
+    /*스타일*/
+    border: none;
+    /*크기*/
+    height: 2.5vw;
+    width: 2.5vw;
+    /*마우스 HOVER 설정*/
+    cursor: pointer;
+    &:active {
+        background: url("/resourcesPng/writeNovelPage/beforePageBtnActive.png") no-repeat;
+        background-size: contain;
+        /*크기*/
+        height: 2.5vw;
+        width: 2.5vw;
+    }
+`;
+const AfterePageBtn = styled.button`
+    background: url("/resourcesPng/writeNovelPage/afterPageBtn.png") no-repeat;
+    background-size: contain;
+    /*스타일*/
+    border: none;
+    /*크기*/
+    height: 2.5vw;
+    width: 2.5vw;
+    /*마우스 HOVER 설정*/
+    cursor: pointer;
+    &:active {
+        background: url("/resourcesPng/writeNovelPage/afterPageBtnActive.png") no-repeat;
+        background-size: contain;
+        /*크기*/
+        height: 2.5vw;
+        width: 2.5vw;
+    }
 `;
 
-const StorageNovelForm = () => {
+const WriteContainer = styled.div`
+    width: 70%;
+    height: 40vw;
+    /*item 정렬*/
+    display: flex;
+    position:relative;
+    justify-content: center;/* 수평  정렬 */
+    align-items: center; /* 수직 중앙 정렬 */
+`;
+const WritePage = styled.div`
+    background: url("/resourcesPng/writeDrawBookPage/whitePage.png") no-repeat center;
+    background-size: contain;
+    /*스타일*/
+    border: none;
+    /*크기*/
+    width: 55vw;
+    height: 40vw;
+    /*레이어*/
+    position:absolute;
+    z-index: 1;
+`;
+const ImageCreateBtn = styled.div`
+    background: url("/resourcesPng/writeDrawBookPage/addImageBtn.png") no-repeat;
+    background-size: contain;
+    /*크기*/
+    height: 10%;
+    width: 10%;
+    /*레이어*/
+    position:absolute;
+    z-index: 2;
+    /*위치*/
+    top: 43%;
+    left:22%;
+
+`;
+const WriteDrawBookForm = () => {
     return (
         <div>
             <Helmet>
-                <title>StorageNovel</title>
-                <meta name="description" content="BlueMemories Introduce Page"/>
+                <title>WriteDrawBook</title>
+                <meta name="description" content="BlueMemories WriteNovel Page"/>
             </Helmet>
 
             <Wrapper>
+                <WriteMenuBar></WriteMenuBar>
+                <BodyContainer>
+                    <BeforePageBtn></BeforePageBtn>
+                    <WriteContainer>
+                        <WritePage></WritePage>
+                        <ImageCreateBtn></ImageCreateBtn>
 
+                    </WriteContainer>
+                    <AfterePageBtn></AfterePageBtn>
+                </BodyContainer>
             </Wrapper>
         </div>
     );
 };
 
-export default StorageNovelForm;
+export default WriteDrawBookForm;
--- BlueMemories/frontend/src/pages/writeNovel/WriteNovelPage.js
+++ BlueMemories/frontend/src/pages/writeNovel/WriteNovelPage.js
@@ -1,8 +1,7 @@
 import {Helmet} from "react-helmet";
 import styled from 'styled-components';
-import { LoggedInNavigationBar } from '../../components/NavigationBar';
-import { BookListItemContainer } from '../../components/BookList';
-import { StoargeTextBtnContainer } from '../../components/StorageTextBtn';
+import React, { useState } from 'react';
+import { WriteMenuBar } from '../../components/WriteMenuBar';
 
 const Wrapper = styled.div`
     width:100%;
@@ -10,48 +9,180 @@ const Wrapper = styled.div`
     display: flex;
     background-color: #FBF5E4;
     flex-direction:column;
-    background-size: cover;
+    align-items: center; /* 수직 중앙 정렬 */
 `;
 
-const MenuBarContainer=styled.div`
-    width:100%;
-    height:7vw;
-    background-image: url("/resourcesPng/writeNovelPage/Menubar2.png");
-    background-size: cover;
-`;
-const ContentContainer=styled.div`
-    height: 80%;
-    width: auto;
+const BodyContainer=styled.div`
+    width: 80%;
+    height:100%;
+    margin-bottom:1%;
+    /*item정렬*/
     display: flex;
-    justify-content: center;
-    margin-top: 3%;
+    justify-content: space-between;/* 수평  정렬 */
+    flex-direction: row;
+    align-items: center; /* 수직 중앙 정렬 */
 `;
-//-------------------------------------------------------------
-
-//가운데 아이템 컨테이너 베이지배경
-const ListBackgroundImg=styled.div`
-    width:47%;
-    height: 34vw;
-    background-image: url("/resourcesPng/storagePage/storage_list_background.png");
+const BeforePageBtn = styled.button`
+    background: url("/resourcesPng/writeNovelPage/beforePageBtn.png") no-repeat;
     background-size: contain;
-    background-position: center;
-    background-repeat: no-repeat;
-    align-items: center;
+    /*스타일*/
+    border: none;
+    /*크기*/
+    height: 2.5vw;
+    width: 2.5vw;
+    /*마우스 HOVER 설정*/
+    cursor: pointer;
+    &:active {
+        background: url("/resourcesPng/writeNovelPage/beforePageBtnActive.png") no-repeat;
+        background-size: contain;
+        /*크기*/
+        height: 2.5vw;
+        width: 2.5vw;
+    }
+`;
+const AfterePageBtn = styled.button`
+    background: url("/resourcesPng/writeNovelPage/afterPageBtn.png") no-repeat;
+    background-size: contain;
+    /*스타일*/
+    border: none;
+    /*크기*/
+    height: 2.5vw;
+    width: 2.5vw;
+    /*마우스 HOVER 설정*/
+    cursor: pointer;
+    &:active {
+        background: url("/resourcesPng/writeNovelPage/afterPageBtnActive.png") no-repeat;
+        background-size: contain;
+        /*크기*/
+        height: 2.5vw;
+        width: 2.5vw;
+    }
 `;
 
-const StorageNovelForm = () => {
+const WriteContainer = styled.div`
+    width: 70%;
+    height: 40vw;
+    /*item 정렬*/
+    display: flex;
+    position:relative;
+    justify-content: center;/* 수평  정렬 */
+    align-items: center; /* 수직 중앙 정렬 */
+`;
+const WritePage = styled.div`
+    background: url("/resourcesPng/writeNovelPage/whitePage.png") no-repeat center;
+    background-size: contain;
+    /*스타일*/
+    border: none;
+    /*크기*/
+    width: 55vw;
+    height: 40vw;
+    /*레이어*/
+    position:absolute;
+    z-index: 1;
+`;
+const ButtonStyled=styled.button`
+    background: url("/resourcesPng/writeNovelPage/addImageBtn.png") no-repeat;
+    background-size: contain;
+    /*스타일*/
+    border: none;
+    /*크기*/
+    height: 10%;
+    width: 10%;
+    /*레이어*/
+    position:absolute;
+    z-index: 3;
+    /*위치*/
+    top: 8%;
+    left: ${(props) => props.marginLeft ||'4.5%'};
+    /*마우스 HOVER 설정*/
+    cursor: pointer;
+`;
+const ImageCreateBtn = ({ isHidden, marginLeft }) => (
+    <ButtonStyled style={{
+        display: isHidden ? 'none' : 'block', marginLeft
+    }} />
+);
+const TextareaStyled = styled.textarea`
+    width: 22.5vw;
+    height: 32vw;
+    /*스타일*/
+    border: none;
+    outline:none;
+    resize: none;
+    /*텍스트 설정*/
+    font-size: 0.8vw;
+    font-family: BokkLight, sans-serif; //대체폰트
+    /*레이어*/
+    position:absolute;
+    z-index: 2;
+    /*위치*/
+    top:12%;
+    left: ${(props) => props.marginLeft || '4.5%'};
+    &:focus {
+        border: none; // 클릭했을 때 테두리 없앰
+        outline:none;
+        resize: none;
+        /*텍스트 설정*/
+        font-size: 0.8vw;
+        font-family: BokkLight, sans-serif; //대체폰트
+    }
+`;
+const WriteTextarea = ({ content, setContent, setIsFocused,marginLeft }) => (
+    <TextareaStyled
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        marginLeft={marginLeft}
+    />
+);
+
+const WriteNovelForm = () => {
+    // 초기값 빈 문자열
+    const [a_content, a_setContent] = useState('');
+    const [b_content, b_setContent] = useState('');
+    // Textarea의 포커스 상태 초기화
+    const [a_isTextareaFocused, a_setIsTextareaFocused] = useState(false);
+    const [b_isTextareaFocused, b_setIsTextareaFocused] = useState(false);
+    // 사용자 입력에 따라 content 상태 업데이트
+    // const handleChange = (e) => {
+    //     setContent(e.target.value);
+    // };
+    //textarea에 포커스가 있을때 && 내용이 적혀있을때 => 그림생성 버튼 hidden
+    const a_isButtonHidden = a_isTextareaFocused || a_content.length > 0;
+    const b_isButtonHidden = b_isTextareaFocused || b_content.length > 0;
+
     return (
         <div>
             <Helmet>
-                <title>StorageNovel</title>
-                <meta name="description" content="BlueMemories Introduce Page"/>
+                <title>WriteNovel</title>
+                <meta name="description" content="BlueMemories WriteNovel Page"/>
             </Helmet>
-
             <Wrapper>
-                <MenuBarContainer></MenuBarContainer>
+                <WriteMenuBar></WriteMenuBar>
+                <BodyContainer>
+                    <BeforePageBtn></BeforePageBtn>
+                    <WriteContainer>
+                        <WritePage></WritePage>
+                        <WriteTextarea
+                            content={a_content}
+                            setContent={a_setContent}
+                            setIsFocused={a_setIsTextareaFocused}
+                        />
+                        <WriteTextarea marginLeft="53.5%"
+                            content={b_content}
+                            setContent={b_setContent}
+                            setIsFocused={b_setIsTextareaFocused}
+                        />
+                        <ImageCreateBtn isHidden={a_isButtonHidden}></ImageCreateBtn>
+                        <ImageCreateBtn isHidden={b_isButtonHidden} marginLeft="49%"></ImageCreateBtn>
+
+                    </WriteContainer>
+                    <AfterePageBtn></AfterePageBtn>
+                </BodyContainer>
             </Wrapper>
         </div>
     );
 };
 
-export default StorageNovelForm;
+export default WriteNovelForm;
